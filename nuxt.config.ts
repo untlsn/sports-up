a56c5dfcca52aab4a54582d53cb53b54fbb12eb4
Filip Skoczeń <philipskoczen@gmail.com>
2025-04-20 16:08:27 +0200

    Add theme to localStorage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,18 @@ export default defineNuxtConfig({
 
   css: ['./app/assets/css/main.css'],
 
+  colorMode: {
+    preference:    'system', // default value of $colorMode.preference
+    fallback:      'light', // fallback value if not system preference found
+    hid:           'nuxt-color-mode-script',
+    globalName:    '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix:   '',
+    classSuffix:   '',
+    storage:       'localStorage', // or 'sessionStorage' or 'cookie'
+    storageKey:    'nuxt-color-mode',
+  },
+
   srcDir: './app',
 
   future: {
